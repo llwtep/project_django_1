119ddc166254ca738aec40c756a2bdf64ba49bc8
llwtep <azamahmedov2005@gmail.com>
2024-08-03 14:05:39 +0500

    add create the posts

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.PostListView.as_view(template_name='main/main.html'), name='main')
+    path('', views.PostListView.as_view(template_name='main/main.html'), name='main'),
+    path('create-post', views.PostCreationView, name='post-create')
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.utils import timezone
-
+from .forms import PostCreateForm
 from .models import Post
 from django.views.generic.list import ListView
 def main(request):
@@ -9,10 +10,21 @@ def main(request):
 
 class PostListView(ListView):
     model = Post
-
+    ordering = ['-created_at']
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
+
+@login_required
+def PostCreationView(request):
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save(user=request.user)
+            return redirect('main')
+    else:
+        form = PostCreateForm()
+    return render(request, 'posts/post-create.html', {'form': form})
 
 
